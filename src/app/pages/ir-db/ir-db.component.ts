27162625af77a4dbc012ee6523a383b735ab3b4d
ixn3rd3mxn <ixn3rd3mxn@gmail.com>
2026-05-29 07:51:20 +0300

    pre-alpha-0.0.14

--- src/app/pages/ir-db/ir-db.component.ts
+++ src/app/pages/ir-db/ir-db.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
-
+import {
+    TuiNavigation
+} from '@taiga-ui/layout';
 @Component({
     selector: 'app-ir-db',
     standalone: true,
+    imports: [
+        TuiNavigation
+    ],
     templateUrl: './ir-db.component.html',
     styleUrl: './ir-db.component.less',
 })
